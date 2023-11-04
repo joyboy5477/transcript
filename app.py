b89ba8dc733bd11c926ec5eddc1ead9ee3529461
Akash Nandlal Vishwakarma <136794783+joyboy5477@users.noreply.github.com>
2023-11-04 02:56:36 -0700

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import os
 from tempfile import NamedTemporaryFile
 
 # Load the Whisper model
-model = whisper.load_model("tiny")
+model = whisper.load_model("small")
 
 st.title("Audio Transcription with OpenAI's Whisper")
 
